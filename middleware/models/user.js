303d97678906264f0c83e21e0e6199bc212963e3
AR.BHARADWAJ <84850843+ARBHARADWAJ@users.noreply.github.com>
2024-03-20 15:48:14 +0530

    Delete middleware directory

--- middleware/models/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose =require("mongoose")
-const userschema=new mongoose.Schema({
-    phno:{type:Number},
-    priority:{type:Number},
-})
-const user=mongoose.model("User",userschema);
-module.exports=user;
